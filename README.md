818d1a2557390cf900e65a2be07269c3aefa1e39
jcgoble3 <jcgoble3@users.noreply.github.com>
2015-09-29 01:27:14 -0400

    Add git prompt code

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # gitstuff
-Various stuff for use with Git
+Various stuff for use with Git. Code is not intended as standalone files, but rather as snippets for use in other files.
+
+All code licensed MIT unless stated otherwise.
+
+## gitprompt.sh
+Code to insert the current branch and status into your bash prompt. Add to ~/.bashrc to use.
+
+Based on:
+* [Original gist](https://gist.github.com/henrik/31631) by [henrik](https://github.com/henrik)
+* [Fork of henrik's gist](https://gist.github.com/srguiwiz/de87bf6355717f0eede5) by [srguiwiz](https://github.com/srguiwiz)
+
+Modified by me, using ideas from those gists and their comments
---
+++ gitprompt.sh
@@ -0,0 +1,60 @@
+# Adds the current branch to the bash prompt when the working directory is
+# part of a Git repository. Includes color-coding and an asterisk to quickly
+# indicate if working directory is clean (green, no asterisk) or dirty (red
+# with asterisk).
+#
+# Based upon the following gists:
+# <https://gist.github.com/henrik/31631>
+# <https://gist.github.com/srguiwiz/de87bf6355717f0eede5>
+# Modified by me, using ideas from comments on those gists.
+#
+# License: MIT, unless the authors of those two gists object :)
+
+git_branch() {
+    # -- Finds and outputs the current branch name by parsing the list of
+    #    all branches
+    # -- Current branch is identified by an asterisk at the beginning
+    # -- If not in a Git repository, error message goes to /dev/null and
+    #    no output is produced
+    git branch --no-color 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+}
+
+git_dirty() {
+    # The --porcelain option produces no output at all if there are
+    # no uncommitted changes. -z tests if the output of the command
+    # is empty; if not empty, dummy text is output. Again, we are not
+    # interested in errors.
+    if [[ ! -z $(git status --porcelain 2>/dev/null) ]]; then
+        echo yes
+    fi
+}
+
+git_color() {
+    # Receives output of git_dirty as argument; produces green color code
+    # if argument is empty and red color code otherwise.
+    if [[ -z $1 ]]; then
+        echo -e '\033[1;32m'  # bold green
+    else
+        echo -e '\033[1;31m'  # bold red
+    fi
+}
+
+git_prompt() {
+    # First, get the branch name...
+    local branch=$(git_branch)
+    # Empty output? Then we're not in a Git repository, so bypass the rest
+    # of the function, producing no output
+    if [[ ! -z $branch ]]; then
+        local dirty=$(git_dirty)
+        local color=$(git_color $dirty)
+        local marker=${dirty:+*}  # asterisk if not empty, nothing otherwise
+        # Now output the actual code to insert the branch and status
+        echo -e "$color[$branch$marker]\033[00m"  # last bit resets color
+    fi
+}
+
+# Sample prompt declaration based off of the default Ubuntu 14.04.1 color
+# prompt. Tweak as you see fit, or just stick "$(git_prompt)" into your
+# favorite prompt.
+PS1='$debian_chroot\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w$(git_prompt)\[\033[00m\]\$ '
+
